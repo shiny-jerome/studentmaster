24451b35007454295a2f9c5dfaf085c11c4b08ca
ABHIRAM <ABHIRAM@DESKTOP-N1VIHBO>
2022-09-15 08:58:54 +0530

    changed student details

--- src/test/java/Student/StudentDetails.java
+++ src/test/java/Student/StudentDetails.java
@@ -4,7 +4,7 @@ public class StudentDetails {
 
 	public static void main(String[] args) {
 		System.out.println("hi");
-		//https://github.com/shiny-jerome/studentmaster.git
+		System.out.println("How are you?");
 
 	}
 
