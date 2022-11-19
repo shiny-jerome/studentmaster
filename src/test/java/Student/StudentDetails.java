1b7c85992e0b58a76f73793a689e6a6721daa0a6
shiny-jerome <shiny.j22@gmail.com>
2022-11-19 08:24:45 +0530

    changed student details

--- src/test/java/Student/StudentDetails.java
+++ src/test/java/Student/StudentDetails.java
@@ -4,6 +4,9 @@ public class StudentDetails {
 
 	public static void main(String[] args) {
 		System.out.println("hi git demo");
+		System.out.println("hi git demo");
+		System.out.println("hi git demo");
+		
 	}
 
 }
